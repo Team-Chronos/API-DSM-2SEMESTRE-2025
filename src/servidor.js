9ef643d97117a146630957d9bca5862eb3229c5e
Enzo Gabriel <enzokof2005@gmail.com>
2025-09-09 22:05:55 -0300

    feat (task 21): implementação dos códigos de verificação de login

--- src/servidor.js
+++ src/servidor.js
@@ -1,17 +1,16 @@
 import express from "express";
 import mysql from "mysql2";
-import bcrypt from "bcrypt";
 import bodyParser from "body-parser";
 
 const app = express();
-app.use(express.static("public")); // serve o index.html
+app.use(express.static("view")); // serve o index.html
 app.use(bodyParser.json());
 
 // conexão com o banco MySQL
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",     // seu usuário do MySQL
-  password: "", // sua senha do MySQL
+  password: "fatec", // sua senha do MySQL
   database: "Api_2"
 });
 
@@ -19,19 +18,19 @@ const db = mysql.createConnection({
 app.post("/login", (req, res) => {
   const { email, senha } = req.body;
 
-  db.query("SELECT * FROM usuarios WHERE email = ?", [email], async (err, results) => {
-    if (err) return res.status(500).json({ mensagem: "Erro no servidor" });
-    if (results.length === 0) return res.json({ mensagem: "Usuário não encontrado" });
+  db.query(
+    "SELECT * FROM usuarios WHERE email = ? AND senha = ?",
+    [email, senha],
+    (err, results) => {
+      if (err) return res.status(500).json({ mensagem: "Erro no servidor" });
 
-    const usuario = results[0];
-    const senhaCorreta = await bcrypt.compare(senha, usuario.senha); // compara senha criptografada
-
-    if (senhaCorreta) {
-      res.json({ mensagem: "Login realizado com sucesso!" });
-    } else {
-      res.json({ mensagem: "Senha incorreta!" });
+      if (results.length > 0) {
+        res.json({ mensagem: "Login realizado com sucesso!" });
+      } else {
+        res.json({ mensagem: "Email ou senha incorretos!" });
+      }
     }
-  });
+  );
 });
 
 // inicia servidor
